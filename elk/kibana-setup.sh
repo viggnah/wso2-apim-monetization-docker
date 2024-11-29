b19e4836ea6a01179b47da97099bed6686e85a82
viggnah <viggnahasd@gmail.com>
2024-11-29 14:37:43 +0530

    Added Kibana setup w/ dashboards

---
+++ elk/kibana-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+KIBANA_URL="http://localhost:5601"
+NDJSON_FILE="./elk/export.ndjson"
+ELASTIC_USER="elastic"
+ELASTIC_PASSWORD="changeme"
+
+# Wait for Kibana to be up
+until $(curl --output /dev/null --silent --head --fail -u $ELASTIC_USER:$ELASTIC_PASSWORD "$KIBANA_URL/api/status"); do
+    echo "Waiting for Kibana to start..."
+    sleep 10
+done
+
+# Delete index patterns
+# for pattern in "apim_event*" "apim_event_faulty" "apim_event_response"; do
+#     echo "Deleting index pattern: $pattern"
+#     curl -X DELETE -u $ELASTIC_USER:$ELASTIC_PASSWORD "$KIBANA_URL/api/saved_objects/index-pattern/$pattern"
+# done
+
+# Import saved objects
+echo "Importing saved objects from ndjson file..."
+curl -X POST "$KIBANA_URL/api/saved_objects/_import?overwrite=true" \
+    -u $ELASTIC_USER:$ELASTIC_PASSWORD \
+    -H "kbn-xsrf: true" \
+    --form file=@"$NDJSON_FILE"
--- start.sh
+++ start.sh
@@ -5,7 +5,9 @@ alias docker=nerdctl
 docker compose down --remove-orphans
 docker compose up -d
 
-# Wait for the WSO2 API Manager to be ready with the PizzaShackAPI
+chmod +x ./elk/kibana-setup.sh
+./elk/kibana-setup.sh
+
 while [[ $(curl -sk -o /dev/null -w "%{http_code}" https://localhost:8300/pizzashack/1.0.0/menu) != 401 ]]; do
     echo "Hold on... waiting for WSO2 API Manager to be ready with the sample PizzaShackAPI"
     sleep 10
@@ -13,5 +15,16 @@ done
 
 chmod +x ./apim/make-requests.sh
 ./apim/make-requests.sh
+
+# echo -e "\n\n--- Service Access Information ---"
+# echo "API Manager"
+# echo "URL: https://localhost:9500/publisher/apis"
+# echo "Username: admin"
+# echo "Password: admin"
+
+# echo -e "\nKibana Dashboard"
+# echo "URL: http://localhost:5601/app/dashboards#/view/f954a940-6ed4-11ec-9007-b93f9eb88870"
+# echo "Username: elastic"
+# echo "Password: changeme"
 
 docker compose logs -f
